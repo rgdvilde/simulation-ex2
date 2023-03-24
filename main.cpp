92d190ed7f44a795ab8b9bd80173348e16d69191
rdve <robbe.de.vilder@gmail.com>
2023-03-24 15:55:51 +0100

    .

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <stdlib.h> 
 
 int nr_fruits = 4;
 FILE *file1;
 char fileName[300];
 int runs = 10000;
-bool smarter_children = false;
+std::string running_average_replication[10000];
+bool smarter_children = true;
+
 
 int fruitsLeft(int fruitArray[], int sizeOfArray) {
     int f = 0;
@@ -28,7 +31,7 @@ float procedure(int k) {
             int i2 = rand()%6;
             if(i2 < nr_fruits) {
                 if(fruits[i2] > 0) {
-                fruits[i2]--;
+                    fruits[i2]--;
                 }
                 else {
                     int i3;
@@ -40,14 +43,14 @@ float procedure(int k) {
                             }
                         }
                         else {
-                            int i3 = rand()%nr_fruits;
+                            i3 = rand()%nr_fruits;
                             while(fruits[i3] == 0) {
                                 i3 = rand()%nr_fruits;
                             }
                         }
                     }
                     else {
-                        int i3 = rand()%nr_fruits;
+                        i3 = rand()%nr_fruits;
                         while(fruits[i3] == 0) {
                             i3 = rand()%nr_fruits;
                         }                        
@@ -57,8 +60,10 @@ float procedure(int k) {
             }
             else {
                 position_raven++;
-                if(fruits[position_raven] > 0) {
-                    fruits[position_raven]--;
+                if(position_raven < 5) {
+                    if(fruits[position_raven-1] > 0) {
+                        fruits[position_raven-1]--;
+                    }
                 }
             }    
         }
@@ -66,7 +71,15 @@ float procedure(int k) {
             total_success++;
         }
         float running_average = ((float)total_success)/(float)(i+1);
-        fprintf(file1, "%f\n", running_average);
+        // fprintf(file1, "%f\n", running_average);
+        // s.append()
+        running_average_replication[i].append(" ");
+        running_average_replication[i].append(std::to_string(running_average));
+        // running_average_replication[i] = running_average_replication + " f"s + std::to_string(running_average);
+        // std::string s = 
+        // strcpy(running_average_replication[i],strcat(running_average_replication[i], " "));
+        // strcpy(running_average_replication[i],std:running_average_replication[i] + ));
+
         final_average = running_average;
     }
     return final_average;
@@ -74,11 +87,28 @@ float procedure(int k) {
 
 int main(int argc, const char * argv[]) {
     strcpy(fileName, "out.txt");
-    file1=fopen(fileName,"w");      
-    float running_average = procedure(runs);
-    std::cout << "Running Average: ";
-    std::cout << std::to_string(running_average);
-    std::cout << "\n";
+    // srand(5); //0.595120
+    // srand(50); //0.597880
+    // srand(455454); //0.584600 smarter: 0.593330
+    int random_numbers[] = {
+    5,50,56565,84754,154845,
+    548478, 4845, 4875, 2256, 547848,
+    88522, 1, 45864, 668, 420,
+    22233366, 44741, 191919, 1564,
+    29, 45454, 555555, 424194, 2
+    };
+    int replication_amount = 25;
+    file1=fopen(fileName,"w");
+    for(int i=0;i<25;i++) {
+        srand(random_numbers[i]);
+        float running_average = procedure(runs);
+        std::cout << "Running Average: ";
+        std::cout << std::to_string(running_average);
+        std::cout << "\n";
+    }
+    for(int i=0;i<runs;i++) {
+        fprintf(file1, "%s\n", running_average_replication[i].c_str());
+    }
     fclose(file1);
     return 0;    
 }
